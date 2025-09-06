9f823cd89347088e7e003af2a27b82f877abd083
BreakerOfHalos <me@breakerofhalos.monster>
2025-09-05 22:24:46 -0700

    Updated config.h and added inject.h (not sure what this one does).

--- keyboards/splitkb/kyria/keymaps/BreakerOfHalos/config.h
+++ keyboards/splitkb/kyria/keymaps/BreakerOfHalos/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2022 Thomas Baart <thomas@splitkb.com>
+/* Copyright 2019 Thomas Baart <thomas@splitkb.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,21 +17,51 @@
 #pragma once
 
 #ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    define RGBLIGHT_EFFECT_RGB_TEST
-#    define RGBLIGHT_EFFECT_ALTERNATING
-#    define RGBLIGHT_EFFECT_TWINKLE
-#    define RGBLIGHT_HUE_STEP 8
-#    define RGBLIGHT_SAT_STEP 8
-#    define RGBLIGHT_VAL_STEP 8
-#    ifndef RGBLIGHT_LIMIT_VAL
-#    	define RGBLIGHT_LIMIT_VAL 150
-#    endif
+#    define RGBLIGHT_DISABLE_KEYCODES
+#    define RGBLIGHT_LAYERS
 #endif
 
+// Home-row mods: https://precondition.github.io/home-row-mods#tap-hold-configuration-settings
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM_PER_KEY
+#ifdef TAPPING_TERM_PER_KEY
+#    define TAPPING_TERM 170
+#else
+// Only for thumbs
+#    define TAPPING_TERM 220
+#endif
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+// Now the default behavior!
+// #define IGNORE_MOD_TAP_INTERRUPT
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+// Apply the modifier on keys that are tapped during a short hold of a modtap
+#define PERMISSIVE_HOLD
+// Immediately turn on layer if key is pressed quickly
+#define HOLD_ON_OTHER_KEY_PRESS
+
+// Combos
+#undef COMBO_TERM
+#define COMBO_TERM 35
+#define COMBO_MUST_TAP_PER_COMBO
+#define COMBO_TERM_PER_COMBO
+// All combos are specified from the base layer, saves space
+#define COMBO_ONLY_FROM_LAYER 0
+
+#define DEFAULT_XCASE_SEPARATOR KC_MINS
+#define XCASE_DELIMITER_KEY KC_UNDS
+
+// Cannot import "keymap_swedish.h" as it sometimes generates extremely weird errors.
+#define LEADER_ESC_KEY KC_E
+
+// Not supported without flto
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
+// Save space
+// #define LAYER_STATE_8BIT
+#define LAYER_STATE_16BIT
+// These are a bit iffy, as I don't know what they're doing, but it "works fine" according to Discord people
+// #undef LOCKING_SUPPORT_ENABLE
+// #undef LOCKING_RESYNC_ENABLE
