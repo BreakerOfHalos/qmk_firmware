0cd8a900c69958742c2ae4251bc0f3b3630e6d16
BreakerOfHalos <me@breakerofhalos.monster>
2025-09-08 00:08:27 -0700

    Added the spider icon to the keymap.

--- keyboards/splitkb/kyria/keymaps/BreakerOfHalos/rules.mk
+++ keyboards/splitkb/kyria/keymaps/BreakerOfHalos/rules.mk
@@ -1,6 +1,6 @@
 OLED_ENABLE = yes
-RGB_MATRIX_ENABLE = yes     # Disable keyboard RGB matrix, as it is enabled by default on rev3
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes
+RGBLIGHT_ENABLE = no      # Disable keyboard RGB underglow
 REPEAT_KEY_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 AUTO_SHIFT_ENABLE = yes
