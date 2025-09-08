81492c3c5f576dd7c2610ee9558756bb734f3d93
BreakerOfHalos <me@breakerofhalos.monster>
2025-09-07 18:10:03 -0700

    Deleted some extra lines from the rules.mk and added copies of Callum's keymap code for the swapper and OSM.

--- keyboards/splitkb/kyria/keymaps/BreakerOfHalos/keymap.c
+++ keyboards/splitkb/kyria/keymaps/BreakerOfHalos/keymap.c
@@ -12,14 +12,11 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-#include <stdint.h>
-#include "quantum.h"
+*/
 #include QMK_KEYBOARD_H
 
 #include "oneshot.h"
-
-#include "g/keymap_combo.h"
+#inclued "swapper.h"
 
 enum layers {
     _BASE = 0,
@@ -147,6 +144,69 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
     }
     return true;
+}
+
+bool is_oneshot_cancel_key(uint16_t keycode) {
+    switch (keycode) {
+    case SYM:
+    case NAV:
+        return true;
+    default:
+        return false;
+    }
+}
+
+bool is_oneshot_ignored_key(uint16_t keycode) {
+    switch (keycode) {
+    case SYM:
+    case NAV:
+    case KC_LSFT:
+    case OS_SHFT:
+    case OS_CTRL:
+    case OS_ALT:
+    case OS_GUI:
+        return true;
+    default:
+        return false;
+    }
+}
+
+bool win_alt_active = false;
+
+oneshot_state os_shft_state = os_up_unqueued;
+oneshot_state os_ctrl_state = os_up_unqueued;
+oneshot_state os_alt_state = os_up_unqueued;
+oneshot_state os_gui_state = os_up_unqueued;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    update_swapper(
+        &win_alt_active, KC_LGUI, KC_TAB, SW_WIN,
+        keycode, record
+    );
+
+    update_oneshot(
+        &os_shft_state, KC_LSFT, OS_SHFT,
+        keycode, record
+    );
+    update_oneshot(
+        &os_ctrl_state, KC_LCTL, OS_CTRL,
+        keycode, record
+    );
+    update_oneshot(
+        &os_alt_state, KC_LALT, OS_ALT,
+        keycode, record
+    );
+    update_oneshot(
+        &os_gui_state, KC_LGUI, OS_GUI,
+        keycode, record
+    );
+
+    return true;
+}
+
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _SYMBOLS, _NAVIGATION, _NUMBERS);
 }
 
 /* The default OLED and rotary encoder code can be found at the bottom of qmk_firmware/keyboards/splitkb/kyria/rev1/rev1.c
--- keyboards/splitkb/kyria/keymaps/BreakerOfHalos/rules.mk
+++ keyboards/splitkb/kyria/keymaps/BreakerOfHalos/rules.mk
@@ -1,8 +1,9 @@
 OLED_ENABLE = yes
-# ENCODER_ENABLE = yes     # Enables the use of one or more encoders, and I don't have any.
 RGB_MATRIX_ENABLE = yes     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-# KEY_OVERRIDE_ENABLE = yes  # Enables key overrides
 REPEAT_KEY_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 AUTO_SHIFT_ENABLE = yes
+
+SRC += oneshot.c
+SRC += swapper.c
