12a61fbd81bba83c578dca87d60f2bd91311b033
BreakerOfHalos <me@breakerofhalos.monster>
2025-09-08 18:22:30 -0700

    Disabling RGB to see if it'll compile for now.

--- keyboards/splitkb/kyria/keymaps/BreakerOfHalos/rules.mk
+++ keyboards/splitkb/kyria/keymaps/BreakerOfHalos/rules.mk
@@ -1,6 +1,8 @@
+CONVERT_TO = liatris
 OLED_ENABLE = yes
-RGB_MATRIX_ENABLE = yes
-RGBLIGHT_ENABLE = no      # Disable keyboard RGB underglow
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+#RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+#WS2812_DRIVER = vendor
 REPEAT_KEY_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 AUTO_SHIFT_ENABLE = yes
